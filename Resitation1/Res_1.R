37d4c2a891e452187dc2cc111512fc7d1bdab94c
zhaohany <zhaohany@outlook.com>
2019-02-01 10:02:15 -0500

    first resitation

--- Resitation1/Res_1.R
+++ Resitation1/Res_1.R
@@ -1,13 +1,14 @@
-#am <- read.csv("Desktop/Data-101/Resitation1/imports-85.data.csv")
-#View(am)
-#am$price (attributes)
+am <- read.csv("Desktop/Data-101/Resitation1/imports-85.data.csv")
+View(am)
+am$price (attributes)
 #replace ? with NA
-#am$price[am$price == '?'] <- NA
-#typeof(am$price)
-#am$price <- as.numeric(am$price)
-#mn <- mean(am$price, na.rm = TRUE)  REMOVE NA from column
-#am$price[is.na(am$price)] <- mn
-#hist(am$price)
-#boxplot(am$price~am$make)
-#barplot(am_make_table)
-#mosaicplot(am$make-am$fuel.type)
+am$price[am$price == '?'] <- NA
+typeof(am$price)
+am$price <- as.numeric(am$price)
+mn <- mean(am$price, na.rm = TRUE)  #REMOVE NA from column
+am$price[is.na(am$price)] <- mn
+hist(am$price)
+boxplot(am$price~am$make)
+am_make_table <- table(am$make)
+barplot(am_make_table)
+mosaicplot(am$make~am$fuel.type)
