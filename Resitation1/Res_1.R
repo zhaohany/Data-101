b91be44e2136f9ee326ee9978d99c5ea8c350a77
zhaohany <zhaohany@outlook.com>
2019-02-01 09:54:57 -0500

    first resitation

---
+++ Resitation1/Res_1.R
@@ -0,0 +1,13 @@
+#am <- read.csv("Desktop/Data-101/Resitation1/imports-85.data.csv")
+#View(am)
+#am$price (attributes)
+#replace ? with NA
+#am$price[am$price == '?'] <- NA
+#typeof(am$price)
+#am$price <- as.numeric(am$price)
+#mn <- mean(am$price, na.rm = TRUE)  REMOVE NA from column
+#am$price[is.na(am$price)] <- mn
+#hist(am$price)
+#boxplot(am$price~am$make)
+#barplot(am_make_table)
+#mosaicplot(am$make-am$fuel.type)
